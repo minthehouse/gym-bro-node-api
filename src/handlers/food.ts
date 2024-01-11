30ed1b741310ad1c355193477fc37536c01994bf
minthehouse <min.navy.lee@gmail.com>
2024-01-11 18:30:58 -0500

    add interfaces to handlers

--- src/handlers/food.ts
+++ src/handlers/food.ts
@@ -1,22 +1,48 @@
 import prisma from "../db";
 
+interface FoodCreateRequestBody {
+  name: string;
+  calories: number;
+  protein: number;
+  carbohydrates: number;
+  fat: number;
+  serving_weight: number;
+  diet_id: number;
+  meal_type_id: number;
+}
+
 export const createFood = async (req, res, next) => {
   try {
+    const {
+      name,
+      calories,
+      protein,
+      carbohydrates,
+      fat,
+      serving_weight,
+      diet_id,
+      meal_type_id,
+    }: FoodCreateRequestBody = req.body;
+
     const food = await prisma.food.create({
       data: {
-        name: req.body.name,
-        calories: req.body.calories,
-        protein: req.body.protein,
-        carbohydrates: req.body.carbohydrates,
-        fat: req.body.fat,
-        meal_type_id: req.body.meal_type_id,
-        serving_weight: req.body.serving_weight,
-        diet_id: req.body.diet_id,
-      },
+        name,
+        calories,
+        protein,
+        carbohydrates,
+        fat,
+        serving_weight,
+        diet: { connect: { id: diet_id } },
+        meal_type: { connect: { id: meal_type_id } },
+      } as any,
       include: {
         diet: true,
         meal_type: true,
       },
     });
-  } catch (e) {}
+
+    res.json({ data: food });
+  } catch (e) {
+    next(e);
+  }
 };
--- src/handlers/muscle_group.ts
+++ src/handlers/muscle_group.ts
@@ -1,10 +1,18 @@
 import prisma from "../db";
 
+interface MuscleGroupCreateRequestBody {
+  name: string;
+  display_name: string;
+}
+
 export const createMuscleGroup = async (req, res, next) => {
   try {
+    const { name, display_name }: MuscleGroupCreateRequestBody = req.body;
+
     const muscleGroup = await prisma.muscleGroup.create({
       data: {
-        name: req.body.name,
+        name,
+        display_name,
       },
     });
 
